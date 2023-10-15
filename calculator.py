d8cc4829f7e43a80c50a920df0d6507253b7fb7c
AlexSvem <alexeydoborin@gmail.com>
2023-10-15 22:47:39 +0300

    version 1.0

---
+++ calculator.py
@@ -0,0 +1,66 @@
+from tkinter import *
+
+fon = "#005C00"
+
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.build()
+
+    def build(self):
+        self.formula = "0"
+        self.lbl = Label(text=self.formula, font=("Enguarda", 21, "bold"), bg=fon, foreground="#FFFF00")
+        self.lbl.place(x=11, y=50)
+
+        btns = [
+            "C", "CE", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "(", "0", ")", "X^2"
+        ]
+
+        x = 10
+        y = 140
+        for bt in btns:
+            com = lambda x=bt: self.logicalc(x)
+            Button(text=bt, bg="#66FFA3",
+                   font=("Enguarda", 15),
+                   command=com).place(x=x, y=y,
+                                      width=115,
+                                      height=79)
+            x += 117
+            if x > 400:
+                x = 10
+                y += 81
+
+    def logicalc(self, operation):
+        if operation == "C":
+            self.formula = ""
+        elif operation == "CE":
+            self.formula = self.formula[0:-1]
+        elif operation == "X^2":
+            self.formula = str((eval(self.formula))**2)
+        elif operation == "=":
+            self.formula = str(eval(self.formula))
+        else:
+            if self.formula == "0":
+                self.formula = ""
+            self.formula += operation
+        self.update()
+
+    def update(self):
+        if self.formula == "":
+            self.formula = "0"
+        self.lbl.configure(text=self.formula)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root["bg"] = fon
+    root.geometry("485x550+200+200")
+    root.title("Calculator")
+    root.resizable(False, False)
+    app = Main(root)
+    app.pack()
+    root.mainloop()
